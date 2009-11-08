52e5fb35a6a9e415b0f3fa50bc495167c32e00d1
Alexander Kahl <e-user@gmx.net>
2009-11-08 21:58:34 +0100

    * yay bootstrapping works!

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# evol - bootstrap evolution
+# Copyright (C) 2009  Alexander Kahl <e-user@fsfe.org>
+# This file is part of evol.
+# evol is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# evol is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+cwd=$(dirname $0)
+cd $cwd
+
+sbcl --noinform --disable-debugger --no-userinit \
+     --eval "(require 'asdf)" \
+     --eval "(push \"${cwd}/\" asdf:*central-registry*)" \
+     --eval "(push \"${HOME}/local/asdf/\" asdf:*central-registry*)" \
+     --eval "(require 'evol)" \
+     --eval "(evol:repl)"
