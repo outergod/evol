f2a1933fc43450dba7d3d78c80088967d92ef933
Alexander Kahl <e-user@gmx.net>
2009-11-15 14:53:25 +0100

    more overdue doc

--- src/path.lisp
+++ src/path.lisp
@@ -17,9 +17,15 @@
 (in-package :evol)
 
 (defun pathname-suffix-p (suffix pathspec)
+  "pathname-suffix-p suffix pathspec => boolean
+
+Predicate whether pathspec has file name suffix."
   (string= suffix (pathname-type (cl-fad:pathname-as-file pathspec))))
 
 (defun pathname-change-suffix (suffix pathspec)
+  "pathname-change-suffix suffix pathspec => string
+
+Change suffix of path name string pathspec; adds suffix if none present yet."
   (let ((pathname (cl-fad:pathname-as-file pathspec)))
     (setf (slot-value pathname 'type) suffix)
     (namestring pathname)))
