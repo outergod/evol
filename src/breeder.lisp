15c71d168c8e9abd7b3bafd9c04ba5993ce1fe96
Alexander Kahl <e-user@fsfe.org>
2009-11-26 21:00:49 +0100

    implemented the swarm (patron queue support outstanding)

--- src/breeder.lisp
+++ src/breeder.lisp
@@ -17,10 +17,23 @@
 (in-package :evol)
 
 ;;; helpers
-(defmacro with-dependency-nodes (&body body)
-  `(let ((nodes (dependency-nodes-hashtable #'evolvable-p #'name #'dependencies *environment*)))
+(defmacro with-dependency-nodes (var &body body)
+  `(let ((,var (dependency-nodes-hashtable #'evolvable-p #'name #'dependencies *environment*)))
      ,@body))
-  
+
+(defmacro eval-reverse-cons ((&body body1) (&body body2))
+  `(let ((result (,@body2)))
+     (cons (,@body1)
+           result)))
+
+(defun safe-getenv (lock var)
+  "safe-getenv lock var => mixed
+
+Mutex-protected GETENV."
+  (bt:with-lock-held (lock)
+    (getenv var :expanded nil)))
+
+
 ;;; breeder class
 (defclass breeder () ()
   (:documentation "Solitary breeder class."))
@@ -28,7 +41,51 @@
 (defgeneric breed (breeder evolvable)
   (:documentation "Breed the evolvable so it can hatch")
   (:method ((breeder breeder) (evol evolvable))
-    (with-dependency-nodes
+    (with-dependency-nodes nodes
       (nreverse (mapcar #'(lambda (name)
                             (evolve (getenv name :expanded nil)))
                         (resolve-queue (find-node (name evol) nodes) nodes))))))
+
+
+;;; swarm class
+(defclass swarm (breeder patron:patron)
+  ((stream :reader   fstream
+           :initarg  :stream
+           :initform *standard-output*
+           :documentation "Stream to use for printing"))
+  (:documentation "Swarms are breeders that heavily rely on threading and also
+derive from Patron for thread-pooled queue working."))
+
+(defgeneric safe-format (swarm lock control-string &rest format-arguments)
+  (:documentation "Mutex lock based FORMAT for swarms.")
+  (:method ((swarm swarm) lock control-string &rest format-arguments)
+    (bt:with-lock-held (lock)
+      (apply #'format (fstream swarm) control-string format-arguments))))
+
+(defmethod breed :around ((swarm swarm) (evol evolvable))
+  (patron:start-patron swarm)
+  (unwind-protect (call-next-method)
+    (patron:stop-patron swarm :wait nil :kill nil)))
+
+(defmethod breed ((swarm swarm) (evol evolvable))
+  "breed swarm evolvable => dag
+
+Swarm-based evolution. TODO: Explain more here"
+  (let ((stream-lock (bt:make-lock "stream"))
+        (env-lock    (bt:make-lock "env")))
+    (labels ((acc (branch)
+                  (if (null branch)
+                      nil
+                    (let* ((evol (safe-getenv env-lock (car branch)))
+                           (evol-lock (mutex evol)))
+                      (bt:with-lock-held (evol-lock)
+                        (if (hatched evol) t
+                          (eval-reverse-cons 
+                           (evolve evol :formatfn #'(lambda (destination control-string &rest format-arguments)
+                                                      (declare (ignore destination))
+                                                      (safe-format swarm stream-lock control-string format-arguments)))
+                           (mapthread #'(lambda (branch)
+                                          (acc branch))
+                                      (cdr branch)))))))))
+     (with-dependency-nodes nodes
+       (acc (resolve-dag (find-node (name evol) nodes) nodes))))))
